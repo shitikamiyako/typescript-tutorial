c94b359c856404f5079f1ef4621e0d4053a02b52
shitikakei <shitikakei@gmail.com>
2021-08-19 04:34:51 +0900

    test

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,17 +1,18 @@
 "use strict";
-// let greet: Function = () => {
-//   console.log('hello, world');
-// }
-// greet = 'hello';
-// greet = () => {
-//   console.log('hello, again');
-// }
-var add = function (a, b, c /*?*/) {
-    if (c === void 0) { c /*?*/ = 10; }
+// 関数に対する型指定は基本的には返り値で何を返すかで決まる
+// 例えば関数に返り値がない場合はvoid型をつける
+var add = function (a, b, c) {
     console.log(a + b);
     console.log(c);
 };
 add(5, 10, 'ninja');
+console.log(add(5, 10, 'ninja'));
+var testculc = function (d, e, f) {
+    if (f === void 0) { f = 'Good Luck'; }
+    var result = d * e;
+    console.log('result' + f);
+};
+console.log(testculc(4, 7));
 var minus = function (a, b) {
     return a + b;
 };
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,19 +1,20 @@
-// let greet: Function = () => {
-//   console.log('hello, world');
-// }
+// 関数に対する型指定は基本的には返り値で何を返すかで決まる
 
-// greet = 'hello';
-
-// greet = () => {
-//   console.log('hello, again');
-// }
-
-const add = (a: number, b: number, c/*?*/: number | string = 10): void => {
+// 例えば関数に返り値がない場合はvoid型をつける
+const add = (a: number, b: number, c: number | string = 10): void => {
   console.log(a + b);
   console.log(c);
 }
 
 add(5, 10, 'ninja');
+console.log(add(5, 10, 'ninja'));
+
+const testculc = (d: number, e:number, f: string = 'Good Luck'): void => {
+  let result = d * e ;
+  console.log('result' + f)
+}
+
+console.log(testculc(4,7));
 
 const minus = (a: number, b: number): number => {
   return a + b;

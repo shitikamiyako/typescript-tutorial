863a3c4573cf97e96c2b876f7d615b8a8aac65b2
shitikakei <shitikakei@gmail.com>
2021-08-21 02:11:03 +0900

    commit

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,22 +1,21 @@
 "use strict";
 // 関数に対する型指定は基本的には返り値で何を返すかで決まる
 // 例えば関数に返り値がない場合はvoid型をつける
-var test = function (str) {
-    console.log(str);
+var add = function (a, b, c) {
+    if (c === void 0) { c = 10; }
+    console.log(a + b);
+    console.log(c);
 };
-test('hello');
-// const add = (a: number, b: number, c: number | string = 10): void => {
-//   console.log(a + b);
-//   console.log(c);
-// }
-// add(5, 10, 'ninja');
-// const testculc = (d: number, e:number, f: string = 'Good Luck'): void => {
-//   let result = d * e ;
-//   console.log('result'+ ' ' + f)
-// }
-// testculc(4,7);
-// const minus = (a: number, b: number): number => {
-//   return a + b;
-// }
-// let result = minus(10,7);
-// console.log(result);
+add(5, 10);
+var testculc = function (d, e, f) {
+    if (f === void 0) { f = 'Good Luck'; }
+    var result = d * e;
+    console.log('result' + ' ' + f);
+};
+testculc(4, 7);
+var minus = function (a, b) {
+    return a + b;
+};
+var result = minus(10, 7);
+console.log(result);
+//# sourceMappingURL=sandbox.js.map
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,28 +1,22 @@
 // 関数に対する型指定は基本的には返り値で何を返すかで決まる
-
-// 例えば関数に返り値がない場合はvoid型をつける
-const test = (str: string):void => {
-  console.log(str);
+// 例えば関数に返り値がない場合はvoid型をつける。勿論引数に型をつけることも可能。
+const add = (a: number, b: number, c: number | string = 10): void => {
+  console.log(a + b);
+  console.log(c);
 }
 
-test('hello');
-// const add = (a: number, b: number, c: number | string = 10): void => {
-//   console.log(a + b);
-//   console.log(c);
-// }
+add(5, 10);
 
-// add(5, 10, 'ninja');
+const testculc = (d: number, e:number, f: string = 'Good Luck'): void => {
+  let result = d * e ;
+  console.log('result'+ ' ' + f)
+}
 
-// const testculc = (d: number, e:number, f: string = 'Good Luck'): void => {
-//   let result = d * e ;
-//   console.log('result'+ ' ' + f)
-// }
+testculc(4,7);
 
-// testculc(4,7);
+const minus = (a: number, b: number): number => {
+  return a + b;
+}
 
-// const minus = (a: number, b: number): number => {
-//   return a + b;
-// }
-
-// let result = minus(10,7);
-// console.log(result);
+let result = minus(10,7);
+console.log(result);

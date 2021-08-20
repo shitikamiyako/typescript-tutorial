af6bb189f05ad48704e62c338749b482e392aacf
shitikakei <shitikakei@gmail.com>
2021-08-21 03:58:04 +0900

    commit

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,12 +1,19 @@
 "use strict";
 // let greet: Function;
-// example 1
+// 関数は作成したら利用する際には当然ながら引数で示された型指定は厳守しないといけない。
 var greet;
+// 正しい例
 greet = function (name, greeting) {
     console.log(name + " says " + greeting);
 };
-// example 2
+// ダメな例
+// greet = (name: string, greeting: number) => {
+//   console.log(`${name} says ${greeting}`);
+// }
+// 以下の例は引数を再定義した場合。これも再定義した引数でオリジナルと同じ型を指定しないとダメ
+// しかし、関数の返り値に対する型指定は改めてやらなくていい、明示的にやるとif-elseなどのときにエラーになる(偽判定だった場合、一度voidを返してからelseへ移り、最終的にelseの実行結果を返す)。
 var calc;
+// 正しい例
 calc = function (numOne, numTwo, action) {
     if (action === 'add') {
         return numOne + numTwo;
@@ -15,8 +22,18 @@ calc = function (numOne, numTwo, action) {
         return numOne - numTwo;
     }
 };
-// example 3
+// ダメな例
+// calc = (numOne: boolean, numTwo: string, action: number): number => {
+//   if (action === 'add') {
+//     return numOne + numTwo;
+//   } else {
+//     return numOne - numTwo;
+//   }
+// }
+// 引数のオブジェクト名を変えるとき、プロパティも揃える必要がある。
 var logDetails;
 logDetails = function (ninja) {
     console.log(ninja.name + " is " + ninja.age + " years old");
 };
+logDetails({ name: 'John', age: 15 });
+//# sourceMappingURL=sandbox.js.map
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,15 +1,21 @@
 // let greet: Function;
 
-// example 1
+// 関数は作成したら利用する際には当然ながら引数で示された型指定は厳守しないといけない。
 let greet: (a: string, b: string) => void;
 
+// 正しい例
 greet = (name: string, greeting: string) => {
   console.log(`${name} says ${greeting}`);
 }
+// ダメな例
+// greet = (name: string, greeting: number) => {
+//   console.log(`${name} says ${greeting}`);
+// }
 
-// example 2
+// 以下の例は引数を再定義した場合。これも再定義した引数でオリジナルと同じ型を指定しないとダメ
+// しかし、関数の返り値に対する型指定は改めてやらなくていい、明示的にやるとif-elseなどのときにエラーになる(偽判定だった場合、一度voidを返してからelseへ移り、最終的にelseの実行結果を返す)。
 let calc: (a: number, b: number, c: string) => number;
-
+// 正しい例
 calc = (numOne: number, numTwo: number, action: string) => {
   if (action === 'add') {
     return numOne + numTwo;
@@ -17,10 +23,23 @@ calc = (numOne: number, numTwo: number, action: string) => {
     return numOne - numTwo;
   }
 }
+// ダメな例
+// calc = (numOne: boolean, numTwo: string, action: number): number => {
+//   if (action === 'add') {
+//     return numOne + numTwo;
+//   } else {
+//     return numOne - numTwo;
+//   }
+// }
 
-// example 3
+// 引数のオブジェクト名を変えるとき、プロパティも揃える必要がある。
 let logDetails: (obj: {name: string, age: number}) => void;
 
-logDetails = (ninja: {name: string, age: number}) => {
+type person = {name: string, age: number};
+
+
+logDetails = (ninja: person) => {
   console.log(`${ninja.name} is ${ninja.age} years old`);
 }
+
+logDetails({name: 'John', age: 15});

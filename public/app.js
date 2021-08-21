50d988846ebf4c71b4ac7968ae0b2daf2eec2408
shitikakei <shitikakei@gmail.com>
2021-08-22 03:10:29 +0900

    commit

--- public/app.js
+++ public/app.js
@@ -1,9 +1,13 @@
 "use strict";
-// classes
+// クラスがあるなら同然、publicとprivateもある。TypeScriptではそれに加えてreadonlyがある。
 var Invoice = /** @class */ (function () {
+    // 前回までのようにこのタイミングでprivateを決めてしまうとインスタンスから当然detail、readonlyプロパティにはアクセスできない。
+    // 正確にはreadonlyアクセスはできるけども、クラスの外からは読み取りしかできないので変更ができない
     // readonly client: string;
     // private details: string;
     // public amount: number;
+    // なので、修飾子を使う場合は以下のようにコンストラクタ内部で定義する。
+    // こうすることでインスタンスから受け取った引数に対して自動的に修飾子と型定義が適用されるという仕組み。
     function Invoice(client, details, amount) {
         this.client = client;
         this.details = details;
@@ -14,22 +18,30 @@ var Invoice = /** @class */ (function () {
     };
     return Invoice;
 }());
+// 以下実用例。Constructorにより、Invoice(client, details, amount);となる。
 var invOne = new Invoice('mario', 'work on the mario website', 250);
 var invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 var invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
+// 試しにリストからオブジェクトにアクセスして値を取り出してみる。当然detailsはprivateなので
 invoices.forEach(function (inv) {
-    console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
+    console.log(inv.client, inv.details, inv.amount, inv.format());
 });
-var form = document.querySelector('.new-item-form');
-console.log(form.children);
-// inputs
-var type = document.querySelector('#type');
-var tofrom = document.querySelector('#tofrom');
-var details = document.querySelector('#details');
-var amount = document.querySelector('#amount');
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
-});
+// const form = document.querySelector('.new-item-form') as HTMLFormElement;
+// console.log(form.children);
+// // inputs
+// const type = document.querySelector('#type') as HTMLInputElement;
+// const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+// const details = document.querySelector('#details') as HTMLInputElement;
+// const amount = document.querySelector('#amount') as HTMLInputElement;
+// form.addEventListener('submit', (e: Event) => {
+//   e.preventDefault();
+//   console.log(
+//     type.value,
+//     tofrom.value,
+//     details.value,
+//     amount.valueAsNumber
+//   );
+// });
+//# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,23 @@
-// classes
+// クラスがあるなら同然、publicとprivateもある。TypeScriptではそれに加えてreadonlyがある。
 class Invoice {
+  // readonlyアクセスはできるけども、クラスの外からは読み取りしかできず、そうなるとインスタンスで生成されたclientプロパティは当然クラスのそれとは別物なのでエラーになってしまう。
+  // しかし、前回のように以下のような形式で書くのは面倒くさい……
+
   // readonly client: string;
   // private details: string;
   // public amount: number;
 
+  // constructor(c: string, d: string, a: number) {
+  //   this.client = c;
+  //   this.details = c;
+  //   this.amount = c;
+  // }
+
+  // なので、修飾子を使う場合は以下のようにコンストラクタ内部で定義すると上記と同義になる。
+  // こうすることでインスタンスから受け取った引数に対して自動的に修飾子と型定義が適用されるという仕組み。
   constructor(
-    readonly client: string, 
-    private details: string, 
+    readonly client: string,
+    private details: string,
     public amount: number,
   ){}
 
@@ -15,6 +26,7 @@ class Invoice {
   }
 }
 
+// 以下実用例。Constructorにより、Invoice(client, details, amount);となる。
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 
@@ -22,28 +34,29 @@ let invoices: Invoice[] = [];
 invoices.push(invOne)
 invoices.push(invTwo);
 
+// 試しにリストからオブジェクトにアクセスして値を取り出してみる。当然detailsはprivateなのでアクセスできない。
 invoices.forEach(inv => {
   console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
 })
 
 
 
-const form = document.querySelector('.new-item-form') as HTMLFormElement;
-console.log(form.children);
+// const form = document.querySelector('.new-item-form') as HTMLFormElement;
+// console.log(form.children);
 
-// inputs
-const type = document.querySelector('#type') as HTMLInputElement;
-const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
-const details = document.querySelector('#details') as HTMLInputElement;
-const amount = document.querySelector('#amount') as HTMLInputElement;
+// // inputs
+// const type = document.querySelector('#type') as HTMLInputElement;
+// const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+// const details = document.querySelector('#details') as HTMLInputElement;
+// const amount = document.querySelector('#amount') as HTMLInputElement;
 
-form.addEventListener('submit', (e: Event) => {
-  e.preventDefault();
+// form.addEventListener('submit', (e: Event) => {
+//   e.preventDefault();
 
-  console.log(
-    type.value, 
-    tofrom.value, 
-    details.value, 
-    amount.valueAsNumber
-  );
-});
+//   console.log(
+//     type.value,
+//     tofrom.value,
+//     details.value,
+//     amount.valueAsNumber
+//   );
+// });

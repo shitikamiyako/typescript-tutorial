99e7b969a6dbffc47bf1654ef654b4d787bd1bb9
shitikakei <shitikakei@gmail.com>
2021-08-22 01:17:24 +0900

    commit

--- public/app.js
+++ public/app.js
@@ -1,10 +1,15 @@
 "use strict";
+// TSでDOMから要素を取得する際はquerySelectorで指定した要素が存在するかのチェックを行う。
+// またその要素がhtmlファイル中に必ずあることがわかっているならば、querySelectorに!マークをつける
 var anchor = document.querySelector('a');
 if (anchor) {
     console.log(anchor.href);
 }
 console.log(anchor.href);
-//const form = document.querySelector('form')!;
+// ダメな例
+// const anchor = document.querySelector('a');
+// console.log(anchor.href);
+// const form = document.querySelector('form')!;
 var form = document.querySelector('.new-item-form');
 console.log(form.children);
 // inputs
@@ -16,3 +21,4 @@ form.addEventListener('submit', function (e) {
     e.preventDefault();
     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
+//# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,40 @@
+// TSでDOMから要素を取得する際はquerySelectorで指定した要素が存在するかのチェックを行う。
+// またその要素がhtmlファイル中に必ずあることがわかっているならば、querySelectorに!マークをつける
 const anchor = document.querySelector('a')!;
 if(anchor) {
   console.log(anchor.href);
 }
 console.log(anchor.href);
 
-//const form = document.querySelector('form')!;
+// ダメな例
+// const anchor = document.querySelector('a');
+// console.log(anchor.href);
+　
+
+// 特定のFormの要素を取得する例。まずは基本。
+// const form = document.querySelector('form')!;
+// ただし、上記では複数Formがあった場合にこまる。ではこれはどうか？ 答えはNO。何故ならこれはHTMLのクラスであり、要素ではないから。
+// const form = document.querySelector('.new-item-form') !;
+// 正しくはこれ。 クラスをキャストすることで要素を引っ張ってくる。この場合はnew-item-formクラスの<form>の要素を返すという意味になる。
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
+// あとは以下のように返ってきた要素から特定の要素を取得したりする。
 console.log(form.children);
 
-// inputs
+
+// inputの場合はこちら。各種inputのIDに対応させる。
 const type = document.querySelector('#type') as HTMLInputElement;
 const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
+// こっちはFormのsubmitイベントで発火する関数。今回はconsole.log()で上記の要素を返すようにしている。
 form.addEventListener('submit', (e: Event) => {
   e.preventDefault();
 
   console.log(
-    type.value, 
-    tofrom.value, 
-    details.value, 
+    type.value,
+    tofrom.value,
+    details.value,
     amount.valueAsNumber
   );
 });

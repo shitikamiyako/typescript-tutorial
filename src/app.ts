0d86f457df65738a079dbb4f451bcb360de94be6
shitikakei <shitikakei@gmail.com>
2021-08-24 23:09:09 +0900

    commit

--- src/app.ts
+++ src/app.ts
@@ -12,8 +12,10 @@ const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
-// list template instance
+// ListTemplateを使って新しくインスタンスを作り、それでli要素作る。
+// まずul要素を作成。必ずあることを示したいので！マークは忘れずに。
 const ul = document.querySelector('ul')!;
+// 次にlistをListTemplateから作成。
 const list = new ListTemplate(ul);
 
 form.addEventListener('submit', (e: Event) => {
@@ -25,6 +27,6 @@ form.addEventListener('submit', (e: Event) => {
   } else {
     doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
   }
-  
+  // 実際のレンダリング部分。listはListTemplateクラスのインスタンスなので当然renderメソッドを使える。
   list.render(doc, type.value, 'end');
 });
--- src/classes/ListTemplate.ts
+++ src/classes/ListTemplate.ts
@@ -1,19 +1,27 @@
+// 出力結果を実際にindex.htmlのどこかにレンダリングしたい
 import { HasFormatter } from "../interfaces/HasFormatter";
 
 export class ListTemplate {
+  // まずはConstructor。型はHTMLUllistElement、つまりul要素であるということを指定。
   constructor(private container: HTMLUListElement){}
 
+  // レンダリング部分
+  // itemにはHasFormatterを実装して、あとは適宜指定。
   render(item: HasFormatter, heading: string, pos: 'start' | 'end'){
+    // li要素を新たに作成。
     const li = document.createElement('li');
-  
+    //同様にh4要素を新たに作成。
     const h4 = document.createElement('h4');
     h4.innerText = heading;
+    // li要素の末尾に上記のh4要素を追加(li配下にh4タグでマークアップするのと同じ)
     li.append(h4);
-
+    // p要素作成
     const p = document.createElement('p');
+    // itemはHasFormatter実装済みなのでメソッドが使える。
     p.innerText = item.format();
+    // li要素の末尾に上記のp要素を追加(つまり、h4タグの下)
     li.append(p);
-
+    // あとは受け取ったpos引数の値によってcontainerの前か末尾かに配置
     if(pos === 'start'){
       this.container.prepend(li);
     } else {

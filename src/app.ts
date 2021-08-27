827b8d010b1e07079f64df29e69094bcfbc72c91
shitikakei <shitikakei@gmail.com>
2021-08-28 01:37:55 +0900

    commit

--- src/app.ts
+++ src/app.ts
@@ -18,29 +18,34 @@ const list = new ListTemplate(ul);
 form.addEventListener('submit', (e: Event) => {
   e.preventDefault();
 
+  // valuesに当たるプロパティはクラスの方で型を指定されてるのでそれに合わせてタプルにも定義をしておく
   let values: [string, string, number];
   values = [tofrom.value, details.value, amount.valueAsNumber];
 
   let doc: HasFormatter;
   if (type.value === 'invoice') {
+    // クラスの引数自体は上でタプルとして定義してあるので...valuesとする
     doc = new Invoice(...values);
   } else {
     doc = new Payment(...values);
   }
-  
+
   list.render(doc, type.value, 'end');
 });
 
-// TUPLES
+// 普通のTuple
 let arr = ['ryu', 25, true];
 arr[0] = false;
 arr[1] = 'yoshi';
 arr = [30, false, 'yoshi'];
 
+// 型定義したタプル
 let tup: [string, number, boolean] = ['ryu', 25, true];
+// tup[0]はstring型なので以下はエラー
 // tup[0] = false;
 tup[0] = 'ken';
 
 let student: [string, number];
+// これも当然エラー
 //student = [23564, 'chun-li'];
 student = ['chun-li', 23564];
